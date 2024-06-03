6e9c31fd739ab84beec4a5a3d6e24c79050415c6
Austen Furutani <furutani24@up.edu>
2024-06-02 18:22:11 -0700

    Cleaned code

--- src/autoreact.py
+++ src/autoreact.py
@@ -5,15 +5,14 @@ from generate import startGen
 from PIL import Image
 import zipfile
 from io import BytesIO
-global PROCESSED
-PROCESSED = False
-global ZIPDIR
-global OLD
-OLD = None
+global HAS_RAN
+HAS_RAN = None
 st.set_page_config(
     page_title="Auto-React by Austen Furutani",
     page_icon="🚀",
 )
+
+#Text Body
 image = Image.open('img/howto.png')
 st.title('Auto-React 🔧')
 st.caption("🤓 Created By: Austen Furutani")
@@ -27,32 +26,31 @@ st.divider()
 
 
 
-
+#Iterates through tempfolder and zips contents
 def zip_directory(directory):
-    print(directory)
     zip_buffer = BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for foldername, subfolders, filenames in os.walk(directory):
             for filename in filenames:
-                print(filename)
                 file_path = os.path.join(foldername, filename)
                 zip_file.write(file_path, os.path.relpath(file_path, directory))
     zip_buffer.seek(0)
     return zip_buffer
+
+#file input section
 st.header("Generate REACT.JS files here")
 files = st.file_uploader("Upload .png layout mock up files here to generate REACT.js files", accept_multiple_files=True)
-if OLD is None:
-    OLD = 1
+
+if HAS_RAN is None:#needed to fix bug, for some reason runs a second time when the download byttin is pressed
+    HAS_RAN = True
     for file in files:
         temp_dir = tempfile.mkdtemp()
         path = os.path.join(temp_dir, file.name)
-        print(path)
         with open(path, "wb") as f:
             f.write(file.getvalue())
 
         with tempfile.TemporaryDirectory() as temp_dir: 
             startGen.start(path, temp_dir)
-            PROCESSED = True
 
             zip_buffer = zip_directory(temp_dir)
             st.download_button(
--- src/generate/filewriter.py
+++ src/generate/filewriter.py
@@ -1,18 +1,18 @@
 import re
 
-# def write_to_file(code, tempdir):
-#     files = split(code)
+
 
 def write_to_file(code, tempdir):
+    #finds start file flag
     seperated= re.split('START_FILE:', code)
-    filenames = []
     for i in seperated:
         start = i.split('\n', 1)
+        #uses regex to find file name
         filename = re.findall(r'\b\w+\.(?:js|css)\b', start[0])
 
         if len(filename) > 0:
+            #finds end file flag
             code = start[1:][0].split('/*END_FILE:',1)
-            # print(code[0])
             file = open(f'{tempdir}/{filename[0]}', 'w')
             file.write(code[0])
             print(f'{tempdir}/{filename[0]}')
